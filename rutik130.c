275eb83feade6032f98412721f49a44ec20989b3
Rutikchodvadiya <133012801+Rutikchodvadiya@users.noreply.github.com>
2023-05-18 17:40:26 +0530

    Add files via upload

---
+++ rutik130.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+main()
+{
+      {
+        int a, b, c, d, big; 
+        printf("n enter value to a, b, c, d,");         
+        scanf("%d %d %d  , &a, &b, &c, &d");
+        }
+       
+        {
+          in(a>b)
+          big =a;
+          big =d;
+        }
+        
+        {
+         else 
+        }
+        
+        {
+           if(c>d
+           big =c;
+           big =d;
+        }
+        
+        {
+         else
+        }
+        
+        {
+          if(b>d)
+          big =b;
+          big =d;
+        }
+        
+        
+              {
+                printf("n the biggest number");
+                }
+        erturn 0;]
+        
+}            
+            
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
