363e772f0d2e5768bd5db0d781f2b6a7a608da9c
Rutikchodvadiya <133012801+Rutikchodvadiya@users.noreply.github.com>
2023-05-18 19:16:32 +0530

    Add files via upload

---
+++ rutik140.c
@@ -0,0 +1,43 @@
+#include<stdi.h>
+main()
+{
+       {
+        int day;
+        printf("m");
+        printf("t");
+        printf("w");
+        printf("t");
+        printf("f");
+        printf("s");
+        printf("s");
+        scanf(%c", &week);
+        } 
+       
+        {
+            case m:
+                printf("Monday");
+                break;
+            case t:
+                printf("Tuesday");
+                break;
+            case w:
+                printf("Wednesday");
+                break;
+            case t:
+                printf("Thursday");
+                break;
+            case f:
+                printf("Friday");
+                break;
+            case s:
+                printf("Saturday");
+                break;
+            case s:
+                printf("Sunday");
+                break;
+            default :
+                printf("Invalid Input");
+                break;
+        }
+        return 0;
+    }
